27b3d97623ccfea46c268558f6b73142ddb9c7fa
David Luecke <daff@neyeon.com>
2015-09-21 14:06:28 -0700

    Adding build.js (#39) and importing can/route/pushtate (#38)

---
+++ app/templates/build.js
@@ -0,0 +1,9 @@
+var stealTools = require("steal-tools");
+
+var buildPromise = stealTools.build({
+  config: __dirname + "/package.json!npm"
+}, {
+  bundleAssets: {
+    infer: false
+  }
+});
--- app/templates/src/app.js
+++ app/templates/src/app.js
@@ -1,6 +1,7 @@
 import AppMap from "can-ssr/app-map";
 import route from "can/route/";
 import 'can/map/define/';
+import 'can/route/pushstate/';
 
 const AppViewModel = AppMap.extend({
   define: {
