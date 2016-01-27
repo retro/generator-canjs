9b0840b257b972b9f1c1ed3a724ab9bdff9f9425
Matthew Phillips <matthew@matthewphillips.info>
2016-01-27 08:13:20 -0500

    Revert "Merge pull request #66 from pYr0x/60-use-package-json"
    This reverts commit d79049fea0ef2c124c031338a33226f3a6cf01bb, reversing
    changes made to b154f6ee419f047072f648d13f11a5247028d30f.

--- app/index.js
+++ app/index.js
@@ -105,6 +105,10 @@ module.exports = generators.Base.extend({
 
       this.prompt(prompts, function (props) {
         this.props = _.extend(this.props, props);
+
+        this.config.set('folder', this.props.folder);
+        this.config.set('name', this.props.name);
+
         done();
       }.bind(this));
     }.bind(this));
--- component/index.js
+++ component/index.js
@@ -1,4 +1,5 @@
 var generators = require('yeoman-generator');
+var fs = require('fs');
 var path = require('path');
 var _ = require('lodash');
 var utils = require('../lib/utils');
@@ -62,24 +63,27 @@ module.exports = generators.Base.extend({
   writing: function () {
 		var isDoneComponent = this.isDoneComponent;
 		var self = this;
-	  var pkgFile = this.destinationPath('package.json');
+		var package = {};
+		// The folder (usually src/)
+		var folder = this.config.get('folder');
+		var appName = this.config.get('name');
 		var parts = this.name.split('/');
 		var name = _.last(parts);
 
-	  // try to read the packeage.json in the project root folder
-	  try {
-		  this.fs.accessSync(pkgFile, this.fs.R_OK);
-	  } catch (e) {
-		  self.log.error("No package.json file not found at "+pkgFile);
-		  process.exit(1);
-	  }
-
-	  var pkg = this.fs.readJSON(pkgFile, {});
-	  var folder = _.get(pkg, 'system.directories.lib');
-	  var appName = _.get(pkg, 'name');
+		if (this.config.get('configMain') != null) {
+			try {
+				fs.accessSync(this.config.get('configMain'), fs.R_OK);
+			} catch (e) {
+				self.log.error("Main config file not found");
+				process.exit(1);
+			}
+			package = JSON.parse(fs.readFileSync(this.config.get('configMain'), 'utf8'));
+			folder = folder ? folder : _.get(package, 'system.directories.lib', folder);
+			appName = appName ? appName : _.get(package, 'name', appName);
+		}
 
 		if (folder == null || appName == null) {
-			self.log.error("The 'name' or 'system.directories.lib' is not specified in your package.json file.");
+			self.log.error("No 'folder' or 'appName' specified. Neither in your .yo-rc.json nor in your package.json file");
 			process.exit(1);
 		}
 
