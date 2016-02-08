3ea9448d49c8a3abd33768d2e848ce7e57697120
James Atherton <james@bitovi.com>
2016-02-08 15:15:48 -0600

    Fixing 'system.directories.lib' requirement. Closes #72

---
+++ test/component.test.js
@@ -0,0 +1,37 @@
+var assert = require('assert');
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var exec = require('child_process').exec;
+var donejsPackage = require('donejs-cli/package.json');
+var npmVersion = require('../lib/utils').npmVersion;
+var fs = require('fs-extra');
+
+function pipe(child) {
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+}
+
+describe('generator-donejs', function () {
+  it('donejs:component', function (done) {
+    var tmpDir;
+
+    helpers.run(path.join(__dirname, '../component'))
+      .inTmpDir(function (dir) {
+        tmpDir = dir;
+        fs.copySync(path.join( __dirname, "tests", "no_directories" ), dir)
+      })
+      .withOptions({
+        skipInstall: true
+      })
+      .withPrompts({
+        name: 'foo/bar',
+        tag: 'foo-bar'
+      })
+      .on('end', function () {
+        assert( fs.existsSync( path.join( tmpDir, "foo", "bar", "bar.js" ) ), "bar.js exists" );
+        assert( fs.existsSync( path.join( tmpDir, "foo", "bar", "bar_test.js" ) ), "bar_test.js exists" );
+        assert( fs.existsSync( path.join( tmpDir, "foo", "bar", "bar.html" ) ), "bar.html exists" );
+        done();
+      });
+  });
+});
