bf04a22583e284c10c0c33a6a9411225945be1dc
David Luecke <daff@neyeon.com>
2016-02-19 13:51:08 -0800

    Move plugin test so that it is not necessary to change

--- plugin/templates/test/test.js
+++ plugin/templates/test/test.js
@@ -1,9 +1,1 @@
-import QUnit from 'steal-qunit';
-import plugin from '../<%= name %>';
-
-QUnit.module('<%= name %>');
-
-QUnit.test('Initialized the plugin', function(){
-  QUnit.equal(typeof plugin, 'function');
-  QUnit.equal(plugin(), 'This is the <%= name %> plugin');
-});
+import '../<%= name %>_test.js';
