a29921d5b6f2cbe28423e42b43d7e84f3c36e598
Matthew Phillips <matthew@matthewphillips.info>
2016-05-31 16:05:02 -0400

    Include `route` in the appviewmodel
    This is because it will be connected to can.route, which always sets the
    route property.

--- app/templates/src/app.js
+++ app/templates/src/app.js
@@ -3,6 +3,7 @@ import route from 'can-route';
 import 'can-route-pushstate';
 
 const AppViewModel = DefineMap.extend({
+  route: "string",
   message: {
     value: 'Hello World!',
     serialize: false
