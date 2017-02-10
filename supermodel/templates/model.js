020da88aceec8d4ece310453dd72daef7e8986f9
Kevin Phillips <kphillips86@gmail.com>
2017-02-10 13:10:43 -0600

    moving algebra, removing idProp, removing can-connect/tag

--- supermodel/templates/model.js
+++ supermodel/templates/model.js
@@ -2,12 +2,7 @@ import DefineMap from 'can-define/map/';
 import DefineList from 'can-define/list/';
 import set from 'can-set';
 import superMap from 'can-connect/can/super-map/';
-import tag from 'can-connect/can/tag/';
 import loader from '@loader';
-
-const algebra = set.Algebra(
-  set.props.id('<%= idProp %>')
-);
 
 const <%= className %> = DefineMap.extend({
   seal: false
@@ -15,21 +10,20 @@ const <%= className %> = DefineMap.extend({
   '<%= idProp %>': '*'
 });
 
+const algebra = set.Algebra(
+  set.props.id('<%= idProp %>')
+);
+
 <%= className %>.List = DefineList.extend({
   '*': <%= className %>
 });
 
 <%= className %>.connection = superMap({
   url: loader.serviceBaseURL + '<%= url %>',
-  idProp: '<%= idProp %>',
   Map: <%= className %>,
   List: <%= className %>.List,
   name: '<%= name %>',
   algebra
 });
-
-<%= className %>.connection.algebra = algebra;
-
-tag('<%= name %>-model', <%= className %>.connection);
 
 export default <%= className %>;
