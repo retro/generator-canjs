2f7e7f263716af1b62eab76c8940b847608b6765
Kevin Phillips <kphillips86@gmail.com>
2017-02-13 13:55:17 -0600

    fixing missed } in supermodel generator

--- supermodel/templates/fixtures/model.js
+++ supermodel/templates/fixtures/model.js
@@ -9,6 +9,6 @@ const store = fixture.store([{
   description: 'Second item'
 }], <%= className %>.connection.algebra);
 
-fixture('<%= url %>/{<%= idProp %>', store);
+fixture('<%= url %>/{<%= idProp %>}', store);
 
 export default store;
